3c98abbde46bc186bef9265d503e22cafc2e8d8d
onesip <info@onesip.nl>
2026-07-27 15:18:58 +0200

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'local-ledger-v1';
+const CORE = ['./', './index.html', './styles.css', './local-api.js', './app.js', './manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+    return response;
+  })));
+});
